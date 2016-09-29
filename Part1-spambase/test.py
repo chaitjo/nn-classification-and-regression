91d6e8bb41ac615fc298f56ffd9b0c37ae323360
Chaitanya <ckjoshi9@gmail.com>
2016-09-30 00:36:12 +0800

    Added preprocessing

--- Part1-spambase/test.py
+++ Part1-spambase/test.py
@@ -2,16 +2,25 @@ import numpy as np
 import tflearn
 
 from tflearn.data_utils import load_csv
-data, labels = load_csv('spambase.data', categorical_labels=True, n_classes=2)
+data, labels = load_csv('spambase.data', has_header=False, categorical_labels=True, n_classes=2)
+
+# Define data preprocessing
+spam_prep = tflearn.data_preprocessing.DataPreprocessing()
+spam_prep.add_featurewise_zero_center()
+spam_prep.add_featurewise_stdnorm()
 
 # Build neural network
-net = tflearn.input_data(shape=[None, 57])
-net = tflearn.fully_connected(net, 32)
-net = tflearn.fully_connected(net, 32)
+net = tflearn.input_data(shape=[None, 57], data_preprocessing=spam_prep)
+net = tflearn.fully_connected(net, 32, activation='linear', bias=True, weights_init='truncated_normal', bias_init='zeroes', regularizer='L2', weight_decay=0.001)
+net = tflearn.fully_connected(net, 32, activation='linear', bias=True, weights_init='truncated_normal', bias_init='zeroes', regularizer='L2', weight_decay=0.001)
+net = tflearn.dropout(net, keep_prob=0.5)
 net = tflearn.fully_connected(net, 2, activation='softmax')
 net = tflearn.regression(net)
 
 # Define model
 model = tflearn.DNN(net)
 # Start training (apply gradient descent algorithm)
-model.fit(data, labels, n_epoch=10, batch_size=16, show_metric=True)
+model.fit(data, labels, n_epoch=10, batch_size=16, show_metric=True
+
+test = [0,0.64,0.64,0,0.32,0,0,0,0,0,0,0.64,0,0,0,0.32,0,1.29,1.93,0,0.96,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.778,0,0,3.756,61,278]
+print model.predict(test)
