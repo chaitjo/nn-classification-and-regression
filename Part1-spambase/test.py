7a56cce64c6ed64764fc189e196b5297bd44d779
Chaitanya Joshi <ckjoshi9@gmail.com>
2016-10-01 23:51:27 +0800

    Fixed shit

--- Part1-spambase/test.py
+++ Part1-spambase/test.py
@@ -24,7 +24,7 @@ dense2 = tflearn.fully_connected(dropout1, 32, activation='relu',
 dropout2 = tflearn.dropout(dense2, keep_prob=0.5)
 
 softmax = tflearn.fully_connected(net, 2, activation='softmax',
-								bias=True, weights_init='truncated_normal', bias_init='zeros'
+								bias=True, weights_init='truncated_normal', bias_init='zeros',
 								regularizer='L2', weight_decay=0.001)
 
 net = tflearn.regression(softmax, optimizer='adam', 
