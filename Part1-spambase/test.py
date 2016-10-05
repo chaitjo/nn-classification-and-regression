e78fef1bb6489585a68745a86a55a6009e056480
chaitjo <ckjoshi9@gmail.com>
2016-10-05 19:06:21 +0800

    udpates

--- Part1-spambase/test.py
+++ Part1-spambase/test.py
@@ -1,46 +1,60 @@
 import numpy as np
 import tflearn
+import sklearn
 
+#Load data
 from tflearn.data_utils import load_csv
 data, labels = load_csv('spambase.data', has_header=False, categorical_labels=True, n_classes=2)
-data = np.array(data).astype(np.float)
 
-# Define data preprocessing
-spam_prep = tflearn.data_preprocessing.DataPreprocessing()
-spam_prep.add_featurewise_zero_center()
-spam_prep.add_featurewise_stdnorm()
+# Data preprocessing
+data = np.array(data).astype(np.float)
+labels = np.array(labels)
+np.random.seed(10)
+shuffle_indices = np.random.permutation(np.arange(len(data)))
+data_shuffled = data[shuffle_indices]
+labels_shuffled = labels[shuffle_indices]
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.33, random_state=42)
+
+featurewise_mean = np.mean(train_data, axis=0)
+featurewise_std = np.std(train_data, axis=0)
 
 # Build neural network
-input = tflearn.input_data(shape=[None, 57], data_preprocessing=spam_prep)
+input = tflearn.input_data(shape=[None, 57])
 
-dense1 = tflearn.fully_connected(input, 32, activation='relu', 
+dense1 = tflearn.fully_connected(input, 50, activation='relu', 
 								bias=True, weights_init='truncated_normal', bias_init='zeros', 
 								regularizer='L2', weight_decay=0.001)
 dropout1 = tflearn.dropout(dense1, keep_prob=0.5)
 
-dense2 = tflearn.fully_connected(dropout1, 32, activation='relu', 
+dense2 = tflearn.fully_connected(dropout1, 30, activation='relu', 
 								bias=True, weights_init='truncated_normal', bias_init='zeros', 
 								regularizer='L2', weight_decay=0.001)
 dropout2 = tflearn.dropout(dense2, keep_prob=0.5)
 
-softmax = tflearn.fully_connected(dropout2, 2, activation='softmax',
+dense3 = tflearn.fully_connected(dropout2, 15, activation='relu', 
+								bias=True, weights_init='truncated_normal', bias_init='zeros', 
+								regularizer='L2', weight_decay=0.001)
+dropout3 = tflearn.dropout(dense3, keep_prob=0.5)
+
+softmax = tflearn.fully_connected(dropout3, 2, activation='softmax',
 								bias=True, weights_init='truncated_normal', bias_init='zeros',
 								regularizer='L2', weight_decay=0.001)
 
 net = tflearn.regression(softmax, optimizer='adam', 
 						loss='categorical_crossentropy', metric='accuracy', 
-						learning_rate=0.001) #, to_one_hot=True, n_classes=2)
+						learning_rate=0.001)
 
 # Define model
 model = tflearn.DNN(net, 
-					tensorboard_verbose=0, tensorboard_dir='runs/summaries', 
-					checkpoint_path='runs/checkpoints', best_checkpoint_path='runs/best_checkpoint', best_val_accuracy=0.8)
+					tensorboard_verbose=0, tensorboard_dir='summaries', 
+					checkpoint_path='checkpoints/checkpoints', best_checkpoint_path='best_checkpoints/best_checkpoint', best_val_accuracy=0.8)
 
 # Start training (apply gradient descent algorithm)
 model.fit(data, labels, 
-			n_epoch=10, batch_size=16, validation_set=0.2, 
-			show_metric=True, shuffle=True, 
-			snapshot_epoch=True, snapshot_step=100)
+			n_epoch=200, batch_size=None, validation_set=[val_data, val_labels], 
+			show_metric=True, snapshot_step=100)
 
 # Test with dummy data
 test = [[0,0.64,0.64,0,0.32,0,0,0,0,0,0,0.64,0,0,0,0.32,0,1.29,1.93,0,0.96,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.778,0,0,3.756,61,278]]
