570b087f508ad923cc0cb58c506de2ff02a230e8
chaitjo <ckjoshi9@gmail.com>
2016-10-06 19:29:51 +0800

    updates

--- Part1-spambase/train-val.py
+++ Part1-spambase/train-val.py
@@ -14,15 +14,13 @@ data_shuffled = data[shuffle_indices]
 labels_shuffled = labels[shuffle_indices]
 
 from sklearn.cross_validation import train_test_split
-train_data, test_data, train_labels, test_labels = train_test_split(data_shuffled, labels_shuffled, test_size = 0.15, random_state=42)
-train_data, val_data, train_labels, val_labels = train_test_split(train_data, train_labels, test_size = 0.15/0.85, random_state=42)
+train_data, test_data, train_labels, test_labels = train_test_split(data_shuffled, labels_shuffled, test_size = 0.2, random_state=42)
 
 # Data preprocessing
 featurewise_mean = np.mean(train_data, axis=0)
 featurewise_std = np.std(train_data, axis=0)
 train_data = (train_data - featurewise_mean)/featurewise_std
 test_data = (test_data - featurewise_mean)/featurewise_std
-val_data = (val_data - featurewise_mean)/featurewise_std
 
 # Build neural network
 input = tflearn.input_data(shape=[None, 57])
@@ -48,15 +46,15 @@ net = tflearn.regression(softmax, optimizer='adam',
 
 # Define model
 model = tflearn.DNN(net, 
-					tensorboard_verbose=0, tensorboard_dir='summaries/train-val-test', 
-					checkpoint_path='checkpoints/checkpoints', best_checkpoint_path='best_checkpoints/train-val-test/best_checkpoint')
+					tensorboard_verbose=0, tensorboard_dir='summaries/train-val', 
+					checkpoint_path='checkpoints/checkpoints', best_checkpoint_path='best_checkpoints/train-val/best_checkpoint')
 
 # Start training (apply gradient descent algorithm)
 model.fit(train_data, train_labels, 
-			n_epoch=20, batch_size=None, validation_set=(val_data, val_labels), 
+			n_epoch=100, batch_size=None, validation_set=(test_data, test_labels), 
 			show_metric=True, snapshot_step=100, run_id="2layer-50-30_relu_adam_online_lr-0.002")
 
-# model.load('best')
-
-# Test
-print model.evaluate(test_data, test_labels)
+# Test with dummy data
+test = [[0,0.64,0.64,0,0.32,0,0,0,0,0,0,0.64,0,0,0,0.32,0,1.29,1.93,0,0.96,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.778,0,0,3.756,61,278]]
+test = (test - featurewise_mean)/featurewise_std
+print model.predict(test)
